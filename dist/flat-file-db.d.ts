241777d1214603559d0c35c47f89f9ab235e4651
Alibek10599 <hodzhabekov@mail.ru>
2022-07-29 16:57:57 +0600

    patch 1.0.9 asynchronous

--- dist/flat-file-db.d.ts
+++ dist/flat-file-db.d.ts
@@ -4,11 +4,11 @@ export declare class FlatFileDb {
     constructor(filePath: string);
     getFilePath(): string;
     checkFileExists(path: string): Promise<boolean>;
-    save<T>(data: Record<T>[]): Promise<void>;
+    save<T>(records: Record<T>[]): Promise<Record<T>[]>;
     getAllRecords<T>(): Promise<Record<T>[]>;
     getRecordById<T>(id: string): Promise<Record<T>>;
-    createRecord<T>(record: Record<T>): Promise<Record<T>>;
-    updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>>;
+    createRecord<T>(record: Record<T>): Promise<Record<T>[]>;
+    updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>[]>;
     deleteRecord(id: string): Promise<void>;
 }
 //# sourceMappingURL=flat-file-db.d.ts.map
--- src/flat-file-db.ts
+++ src/flat-file-db.ts
@@ -10,7 +10,6 @@ export class FlatFileDb {
       this.filePath = filePath;
     }
     
-    
     getFilePath(): string {
       return this.filePath;
     }
@@ -22,56 +21,54 @@ export class FlatFileDb {
       return true;
     }
   
-    async save<T>(data: Record<T>[]): Promise<void> {
+    async save<T>(records: Record<T>[]): Promise<Record<T>[]> {
       return writeFile(
         this.filePath,
-        JSON.stringify(data),
-      ).then(() => {data});
+        JSON.stringify(records)
+      ).then(() => records)
     }
   
     async getAllRecords<T>(): Promise<Record<T>[]> {
-        const records = JSON.parse(
-            await readFile(this.filePath, 'utf8')
-          ) as Record<T>[];
-          return records;
+     return readFile(this.filePath)
+     .then((body): Record<T>[] => JSON.parse(body.toString()) as Record<T>[]);
     }
   
     async getRecordById<T>(id: string): Promise<Record<T>> {
-      const record = (await this.getAllRecords<T>()).find(
+      return this.getAllRecords<T>().then((records) => records.find(
         (record) => record.id === id
-      );
-      if (!record) throw new Error(`Record with id ${id} not found`);
-      return record;
+      )).then((record) => {
+        if (!record) throw new Error(`Record with id ${id} not found`);
+        return record
+      });
     }
   
-    async createRecord<T>(record: Record<T>): Promise<Record<T>> {
-      const records = await this.getAllRecords();
-      const id: number = records.length + 1;
-      record.id = id.toString();
-        records.push(record);
-      await this.save(records);
-      return record;
+    async createRecord<T>(record: Record<T>): Promise<Record<T>[]> {
+     return this.getAllRecords<T>().then((records) => {
+        record.id = (records.length + 1).toString();
+        return [...records, record];
+      }).then((newRecords) => this.save(newRecords));
     }
   
-    async updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>> {
-      const records = await this.getAllRecords();
-      const recordIndex = records.findIndex((record) => record.id === id);
-      if (recordIndex === -1) {
-        throw new Error(`Record with id ${id} not found`);
-      }
-      records[recordIndex] = record;
-      await this.save(records);
-      return record;
+    async updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>[]> {
+      return this.getAllRecords<T>().then((records) => {
+        return records.map(r => {
+          if(r.id == id){
+            return record; 
+          }
+          return r;
+        });
+      })
+      .then((newRecords) => this.save(newRecords))
     }
   
     async deleteRecord(id: string): Promise<void> {
-      const records = await this.getAllRecords();
-      const record = records.find((record) => record.id === id);
-      if (!record) {
-        throw new Error(`Record with id ${id} not found`);
-      }
-      records.splice(records.indexOf(record), 1);
-      await this.save(records);
+      this.getAllRecords().then((records) => {
+        return records.map(r => {
+          if(r.id != id){ 
+            return r;
+          }
+        });
+      })
+      .then((newRecords) => this.save(newRecords))
     }
   }
-  
