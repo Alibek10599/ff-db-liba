a26bbc57dc8a7d60bbe7b1e20529fabc51269c06
Alibek10599 <hodzhabekov@mail.ru>
2022-08-01 12:51:06 +0600

    1.0.10 remove promices

--- dist/flat-file-db.d.ts
+++ dist/flat-file-db.d.ts
@@ -4,11 +4,11 @@ export declare class FlatFileDb {
     constructor(filePath: string);
     getFilePath(): string;
     checkFileExists(path: string): Promise<boolean>;
-    save<T>(records: Record<T>[]): Promise<Record<T>[]>;
+    save<T>(data: Record<T>[]): Promise<void>;
     getAllRecords<T>(): Promise<Record<T>[]>;
     getRecordById<T>(id: string): Promise<Record<T>>;
-    createRecord<T>(record: Record<T>): Promise<Record<T>[]>;
-    updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>[]>;
-    deleteRecord(id: string): Promise<void>;
+    createRecord<T>(record: Record<T>): Promise<Record<T>>;
+    updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>>;
+    deleteRecord(id: string): Promise<string>;
 }
 //# sourceMappingURL=flat-file-db.d.ts.map
--- src/flat-file-db.ts
+++ src/flat-file-db.ts
@@ -1,7 +1,9 @@
 import * as fs from 'fs';
-import { writeFile, readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 import { Record } from './interface';
+
+var SIZE = 64;
 
 export class FlatFileDb {
     public filePath: string;
@@ -16,59 +18,65 @@ export class FlatFileDb {
   
     async checkFileExists(path: string): Promise<boolean> {
       if (!fs.existsSync(path)) {
-        fs.writeFileSync(path, '');
+        await fs.writeFile(path, '[]', (err) => {
+            if (err) throw err;
+        });
+      } else {
+        return true;
       }
-      return true;
     }
   
-    async save<T>(records: Record<T>[]): Promise<Record<T>[]> {
-      return writeFile(
+    async save<T>(data: Record<T>[]): Promise<void> {
+      await writeFile(
         this.filePath,
-        JSON.stringify(records)
-      ).then(() => records)
+        JSON.stringify(data)
+      );
     }
   
     async getAllRecords<T>(): Promise<Record<T>[]> {
-     return readFile(this.filePath)
-     .then((body): Record<T>[] => JSON.parse(body.toString()) as Record<T>[]);
+        const records = JSON.parse(
+            await readFile(this.filePath, 'utf8')
+          ) as Record<T>[];
+          return records;
     }
   
     async getRecordById<T>(id: string): Promise<Record<T>> {
-      return this.getAllRecords<T>().then((records) => records.find(
+      const record = (await this.getAllRecords<T>()).find(
         (record) => record.id === id
-      )).then((record) => {
-        if (!record) throw new Error(`Record with id ${id} not found`);
-        return record
-      });
+      );
+      if (!record) throw new Error(`Record with id ${id} not found`);
+      return record;
     }
   
-    async createRecord<T>(record: Record<T>): Promise<Record<T>[]> {
-     return this.getAllRecords<T>().then((records) => {
-        record.id = (records.length + 1).toString();
-        return [...records, record];
-      }).then((newRecords) => this.save(newRecords));
+    async createRecord<T>(record: Record<T>): Promise<Record<T>> {
+      const records = await this.getAllRecords();
+      const id: number = records.length + 1;
+      record.id = id.toString();
+        records.push(record);
+      await this.save(records);
+      return record;
     }
   
-    async updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>[]> {
-      return this.getAllRecords<T>().then((records) => {
-        return records.map(r => {
-          if(r.id == id){
-            return record; 
-          }
-          return r;
-        });
-      })
-      .then((newRecords) => this.save(newRecords))
+    async updateRecord<T>(id: string, record: Record<T>): Promise<Record<T>> {
+      const records = await this.getAllRecords();
+      const recordIndex = records.findIndex((record) => record.id === id);
+      if (recordIndex === -1) {
+        throw new Error(`Record with id ${id} not found`);
+      }
+      records[recordIndex] = record;
+      await this.save(records);
+      return record;
     }
   
-    async deleteRecord(id: string): Promise<void> {
-      this.getAllRecords().then((records) => {
-        return records.map(r => {
-          if(r.id != id){ 
-            return r;
-          }
-        });
-      })
-      .then((newRecords) => this.save(newRecords))
+    async deleteRecord(id: string): Promise<string> {
+      const records = await this.getAllRecords();
+      const record = records.find((record) => record.id === id);
+      if (!record) {
+        throw new Error(`Record with id ${id} not found`);
+      }
+      records.splice(records.indexOf(record), 1);
+      await this.save(records);
+      return id;
     }
   }
+  
